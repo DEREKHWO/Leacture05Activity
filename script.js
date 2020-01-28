ea107c98439c4184c72560a65b9b5aa7f7581204
Derrek Meno <DERREKWHO15@gmail.com>
2020-01-27 19:37:14 -0800

    defined pi and myNumber

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let myGreeting = "Whats poppin bro" ;
 
 
-console.log("myGreeting");
+console.log(myGreeting);
+
+
+const pi = 3.1415
+
+//pi = 2  will not let pi be redefined as it is already set to permantly be 3.1415
+
+let myNumber = 13
+
+console.log(Math.min(pi, myNumber)); // outputs pi as pi is smaller than myNumber
+
+
+
